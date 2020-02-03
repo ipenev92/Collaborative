bc2dd4e25a8555451bfe52f9bed74a693fc81e73
Ivan Penev <ivan.penev92@gmail.com>
2020-02-03 23:03:48 +0100

    fixed load + active

--- website/javascript/script.js
+++ website/javascript/script.js
@@ -1,26 +1,17 @@
-window.onload = function() { // Onload
-	let url   = window.location.href,
-		pages = ["About", "Contact", "Index", "Lawyers", "Practices"];
-	active(pages, url);
-}
-
 $(document).ready(() => {
 	let header = "http://ivanpenev.hopto.org/bootstrap/website/resources/header.html",
     	footer = "http://ivanpenev.hopto.org/bootstrap/website/resources/footer.html";
 
-    // Load the HTML contents of the header into <header>
-    $('header').load(header);
-    // Load the HTML contents of the footer into <footer>
+    // Load header, then apply active
+	$('header').load(header, function() {
+	    let page = window.location.href.split("/").slice(-1)[0].split(".")[0]; //get file name
+	    active(page);
+ 	});
+
+    // Load footer
     $('footer').load(footer);
 });
 
-/* Active Class */
-const active = (pages, url) => {
-	let arr = url.split("/"),
-		page = arr[arr.length-1].split(".")[0];
-
-	console.log(arr);
-	console.log(page);
-
+const active = (page) => {
 	document.getElementById(page).setAttribute('class', 'active');
 }
