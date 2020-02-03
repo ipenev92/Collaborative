3e73a6749feb157d5e698f5904f90075d4256777
Ivan Penev <ivan.penev92@gmail.com>
2020-02-03 20:29:11 +0100

    updating header.html

---
+++ lawfirm website/Javascript/script.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    // Load the HTML contents of the header into <header>
+    $('header').load('https://raw.githubusercontent.com/ipenev92/Bootstrap/master/lawfirm%20website/resources/header.html?token=AGNJPYAYSI2RCYE25TLVYSS6HBZ2U');
+    // Load the HTML contents of the footer into <footer>
+    $('footer').load('https://raw.githubusercontent.com/ipenev92/Bootstrap/master/lawfirm%20website/resources/footer.html?token=AGNJPYFYYKO5UJSEWNGF33C6HBZ24');
+});
