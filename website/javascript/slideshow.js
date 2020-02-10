8ddb432a17cc411ef2053bae8de52d9513da4059
Ivan Penev <ivan.penev92@gmail.com>
2020-02-10 14:13:01 +0100

    about bootstrapped + slideshow

---
+++ website/javascript/slideshow.js
@@ -0,0 +1,28 @@
+let slideIndex = 0,
+    slides = document.getElementsByClassName("slideshow"),
+    dots = document.getElementsByClassName("dot");
+
+const showSlides = () => {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    const timeout = setTimeout(showSlides, 5000);
+}
+
+const changeSlide = n => {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n].style.display = "block";
+    dots[n].className += " active";
+}
